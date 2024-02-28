e470a4482dbd188aa28156e39f64c3662c9b8b7b
Gabriel Rodrigues <contatogabrielrodrigues20@gmail.com>
2024-02-28 19:09:51 -0300

    Signed-off-by: Gabriel Rodrigues <contatogabrielrodrigues20@gmail.com>

---
+++ Diagramaclassejogo/entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class entity:
+    def __init__(self):
+        self.name_m = None
+        self.surf_Surface = None
+        self.rect_Rect = None
+
+    def move(self, ):
+        pass
+
